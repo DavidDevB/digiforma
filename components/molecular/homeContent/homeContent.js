f86221f0da72f74bc6fcae7a525a7aaddd936868
DavidB <david.bernard@fms-ea.com>
2026-01-13 11:07:13 +0100

    commit before switch branch

---
+++ components/molecular/homeContent/homeContent.js
@@ -0,0 +1,21 @@
+
+
+const homeContent = () => {
+    return (`<div class="HomeContent">
+                <h2>Bienvenue à la Prépa Concepteur Développeur d'application
+
+"PYTHON / JAVA" - Promotion 1</h2>
+                <p>Cette formation vous permet d'avoir une remise à niveau en vue d’intégrer le parcours de formation : « CONCEPTEUR DEVELOPPEUR D’APPLICATION » sur les technologies « Java Spring et Angular »
+
+  
+La formation se déroulera en présentiel et en distanciel.
+
+
+
+Lieux de formation : 549 rue du Marais d'Orx - 40230 SAINT-GEOURS-DE-MAREMNE
+
+                                 2 rue Louis Courtois de Vicose - 31100 TOULOUSE
+
+                                 3 allée Pierre Gilles de Gennes - 33700 MERIGNAC</p>
+            </div>`);
+}
